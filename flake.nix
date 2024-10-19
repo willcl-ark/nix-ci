7ddd126475832855e25ef88eadc9d6f5354ead8f
0xb10c <b10c@b10c.me>
2024-10-19 11:51:01 +0200

    drop unused nixos-facter-modules input

--- flake.nix
+++ flake.nix
@@ -2,13 +2,11 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
   outputs =
     {
       nixpkgs,
       disko,
-      nixos-facter-modules,
       ...
     }:
     {
